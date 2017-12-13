50cb9553287eb7573c9d7fbc97d705048e6ef06a
Trey Huffine <trey@postmates.com>
2017-12-12 18:01:39 -0800

    New components

--- hacker-news-reader/src/views/pages/Page/Page.js
+++ hacker-news-reader/src/views/pages/Page/Page.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Navbar from 'components/Navbar';
+import Footer from 'components/Footer';
 
 const Page = ({ pageName, children }) => (
   <div className={pageName}>
-    <Navbar />
-    {children}
+    <Navbar/>
+    <main>
+      {children}
+    </main>
+    <Footer/>
   </div>
 );
 
