a7e5a1a30777d458c5c2471984cba2b85de42f22
Trey Huffine <treyhuffine@gmail.com>
2017-12-15 07:27:46 -0800

    Typo

--- hacker-news-reader/src/views/Root.jsx
+++ hacker-news-reader/src/views/Root.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from 'routes/index';
 
-import 'styles/global';
+import 'styles/globals';
 
 class Root extends Component {
   static propTypes = {
